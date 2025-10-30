f2268d2a835e27a0794871ff8eb001ae33e3a154
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2025-10-30 22:45:45 +0900

    発射処理できました

--- Assets/Scripts/InGame/Weapon/SingleShot.cs
+++ Assets/Scripts/InGame/Weapon/SingleShot.cs
@@ -24,7 +24,7 @@ namespace MoreSpace.InGame.Weapons
         {
             if(!CanShot()) return;
             var instance =  pool.GetPooledObject();
-            instance.transform.position = this.transform.position + this.transform.forward;
+            instance.transform.position = this.transform.position + this.transform.forward*20;
             instance.Shot(CalcTargetPosition(),speed,damage);
             SetNextFireTime();
         }
