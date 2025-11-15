0a65d4e13178d48161c0b35f645eec8cfe693ea0
h-ryushin <115770963+h-ryushin@users.noreply.github.com>
2025-11-15 11:55:57 +0900

    シングルショットバフ完了

--- Assets/Scripts/InGame/Weapon/SingleShot.cs
+++ Assets/Scripts/InGame/Weapon/SingleShot.cs
@@ -11,7 +11,7 @@ namespace MoreSpace.InGame.Weapons
         [SerializeField] public float speed = 10;
         [SerializeField] public int damage = 10;
 
-        [SerializeField] public int ObjectDamage = 0;
+        [SerializeField] public int ObjectDamage = 10;
 
         private Pool<SingleBullet> pool;
         private PlayerBuffs _buffs;
@@ -26,10 +26,14 @@ namespace MoreSpace.InGame.Weapons
         public override void OnFireDown()
         {
             if (!CanShot()) return;
+            // プレイヤーのバフ情報を取得
+            _buffs = this.gameObject.GetComponentInParent<PlayerBuffs>();
             
             // 1) バフの取得（なければ 0）
             float atkBonus    = _buffs != null ? _buffs.Attack          : 0f;
             float objAtkBonus = _buffs != null ? _buffs.AttackForObject : 0f;
+            Debug.Log(atkBonus+"これプレイヤー攻撃のバフ");
+            Debug.Log(objAtkBonus+"これオブジェクト攻撃のバフ");
 
             // 2) 最終ダメージを算出（加算）
             int finalDamage       = damage       + Mathf.RoundToInt(atkBonus);
@@ -37,7 +41,7 @@ namespace MoreSpace.InGame.Weapons
 
             var instance =  pool.GetPooledObject();
             instance.transform.position = this.transform.position + this.transform.forward*20;
-            instance.Shot(CalcTargetPosition(),speed,finalDamage,this.gameObject,photonView.IsMine);
+            instance.Shot(CalcTargetPosition(),speed,finalDamage,finalObjectDamage,this.gameObject,photonView.IsMine);
             SetNextFireTime();
         }
 
