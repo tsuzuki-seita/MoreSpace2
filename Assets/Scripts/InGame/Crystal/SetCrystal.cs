a3208450c22a080e6c19516e28dd87ec5b181f57
Break <hsmtyuusei@gmail.com>
2025-11-22 21:39:14 +0900

    クリスタルの位置固定/回転でランダム性を出すように変更

--- Assets/Scripts/InGame/Crystal/SetCrystal.cs
+++ Assets/Scripts/InGame/Crystal/SetCrystal.cs
@@ -5,18 +5,9 @@ namespace MoreSpace.InGame
 {
     public class SetCrystal : MonoBehaviour
     {
-        [SerializeField] private GameObject crystalPrefab;
-        [SerializeField] private float makeRange;
-
-        void Start()
+        void Awake()
         {
-            if(!PhotonNetwork.IsMasterClient) return;
-            Vector3 makeVector = Random.onUnitSphere.normalized;
-            var crystal = PhotonNetwork.Instantiate(crystalPrefab.name,Vector3.zero, Quaternion.identity);
-            var scaleOffset = this.transform.localScale.x;
-            crystal.transform.localScale = Vector3.one * 75 * scaleOffset;
-            crystal.transform.localPosition = this.transform.position + makeVector * makeRange * scaleOffset;
-            crystal.transform.LookAt(this.transform);
+            this.transform.rotation = Random.rotationUniform;
         }
     }
 }
