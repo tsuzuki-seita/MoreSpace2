1ffb8ce218c09d22788675adc472cdb872499e67
Break <hsmtyuusei@gmail.com>
2025-11-24 20:03:54 +0900

    ビームの作成/アセットの追加

--- Assets/Scripts/Domain/WeaponBeamSkill.cs
+++ Assets/Scripts/Domain/WeaponBeamSkill.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
-using MoreSpace.Domain;
+using MoreSpace.InGame.Weapons.Bullets;
 
-[CreateAssetMenu(fileName = "NewWeaponBeamSkill", menuName = "MoreSpace/WeaponBeamSkill")]
-public sealed class WeaponBeamSkill : WeaponSkill
+namespace MoreSpace.InGame.Weapons
 {
-    [Header("Weapon Data")]
-    public float ReleaseDurationTime;
-
-    public override void Initialize(GameObject owner)
+    [CreateAssetMenu(fileName = "NewWeaponBeamSkill", menuName = "MoreSpace/WeaponBeamSkill")]
+    public sealed class WeaponBeamSkill : WeaponSkill
     {
-        owner.GetComponent<SkillViewer>().ActivateSkillUI(this, null);
-        // 武器をプレイヤーにアタッチするなどの準備
-        Debug.Log($"{SkillName} initialized on {owner.name}");
+        [Header("Weapon Data")] public float ReleaseDurationTime;
+
+        public override void Initialize(GameObject owner)
+        {
+            var laser = owner.AddComponent<MoreSpace.InGame.Weapons.Beam>();
+
+            laser.bulletPrefab = ProjectilePrefab.GetComponent<BeamBullet>();
+            laser.maxDistance = Distance;
+            laser.damage = Mathf.RoundToInt(Damage);
+            laser.fireRate = RecastTime;
+            laser.maxBeamDuration = ReleaseDurationTime;
+
+            // ControlWeapon へレベル順で登録
+            var cw = owner.GetComponent<ControlWeapon>();
+            if (cw != null) cw.AddWeapon(laser);
+
+            owner.GetComponent<SkillViewer>().ActivateSkillUI(this, laser);
+            Debug.Log($"{SkillName} initialized on {owner.name}");
+        }
     }
 }
